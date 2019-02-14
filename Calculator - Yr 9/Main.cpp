6d295ba1b0d39560b520ec44e89ec09322ad46f2
Lorenzo1711 <shepl084.309@fortismere.school>
2019-02-14 15:02:27 +0000

    Create Main.cpp

---
+++ Calculator - Yr 9/Main.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+
+        case '-':
+            cout << num1-num2;
+            break;
+
